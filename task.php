39d97fd0be549a4f6317ac883dc43a6d9aa4d05a
RedGuy12 <paul@reid-family.org>
2021-02-15 13:05:46 -0600

    Updates from heroku

--- task.php
+++ task.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * MyBB 1.6
  * Copyright 2010 MyBB Group, All Rights Reserved
@@ -17,55 +18,44 @@ define("NO_ONLINE", 1);
 define("IN_TASK", 1);
 define('THIS_SCRIPT', 'task.php');
 
-require_once dirname(__FILE__)."/inc/init.php";
+require_once dirname(__FILE__) . "/inc/init.php";
 
 // Load language
 $lang->set_language($mybb->settings['bblanguage']);
 $lang->load("global");
 $lang->load("messages");
 
-if(function_exists('mb_internal_encoding') && !empty($lang->settings['charset']))
-{
+if (function_exists('mb_internal_encoding') && !empty($lang->settings['charset'])) {
 	@mb_internal_encoding($lang->settings['charset']);
 }
 
-require_once MYBB_ROOT."inc/functions_task.php";
+require_once MYBB_ROOT . "inc/functions_task.php";
 
 // Are tasks set to run via cron instead & are we accessing this file via the CLI?
 // php task.php [tid]
-if(PHP_SAPI == "cli")
-{
+if (PHP_SAPI == "cli") {
 	// Passing a specific task ID
-	if($_SERVER['argc'] == 2)
-	{
-		$query = $db->simple_select("tasks", "tid", "tid='".intval($_SERVER['argv'][1])."'");
+	if ($_SERVER['argc'] == 2) {
+		$query = $db->simple_select("tasks", "tid", "tid='" . intval($_SERVER['argv'][1]) . "'");
 		$tid = $db->fetch_field($query, "tid");
 	}
 
-	if($tid)
-	{
+	if ($tid) {
 		run_task($tid);
-	}
-	else
-	{
+	} else {
 		run_task();
 	}
 }
 // Otherwise FALSE GIF image, only supports running next available task
-else
-{
+else {
 	// Send our fake gif image (clear 1x1 transparent image)
 	header("Content-type: image/gif");
 	echo base64_decode("R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
 
 	// If the use shutdown functionality is turned off, run any shutdown related items now.
-	if($mybb->use_shutdown == TRUE)
-	{
+	if ($mybb->use_shutdown == TRUE) {
 		add_shutdown("run_task");
-	}
-	else
-	{
+	} else {
 		run_task();
 	}
 }
-?>
