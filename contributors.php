34cd35162823f6aae4b6f29237ab4cdd551c9d79
RedGuy12 <paul@reid-family.org>
2021-02-19 15:12:36 -0600

    .

--- contributors.php
+++ contributors.php
@@ -1,5 +1,5 @@
 <?php
-
+$time_start = microtime(TRUE);
 define("IN_MYBB", 1);
 define('THIS_SCRIPT', 'contributors.php');
 //require_once "./global.php";
@@ -7,39 +7,27 @@ define('THIS_SCRIPT', 'contributors.php');
 
 $org = "scratchdit";
 
-function fetch($url)
-{
-	$ch = curl_init();
-	if ($ch === FALSE) {
-		echo "init failed";
-	}
-
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'RedGuy12');
-	curl_setopt($ch, CURLOPT_COOKIE, "user_session=G9FxEJnxYRJuYeRDHMEiVHWdKamTuHHVxZyOi8b_Jzl_GqU6");
-	$out = curl_exec($ch);
-	curl_close($ch);
-	if ($out === FALSE) {
-		$out  = curl_error($ch);
-		$out .= " ";
-		$out .= curl_errno($ch);
-	}
-
-	unset($ch);
-	return $out;
-}
-
-preg_match_all("/f=\"\/orgs\/$org\/people\/(.*)\"></", fetch("https://github.com/orgs/$org/people"), $people);
+$people_html = fetch("https://github.com/orgs/$org/people");
+preg_match_all("/f=\"\/orgs\/$org\/people\/(.*)\"></", $people_html, $people);
 foreach ($people[1] as $i => $person) {
 	$contributors[$person]["💼"] = 1;
 }
 
-unset($person);
-unset($i);
+$count = preg_match_all('/\d*<\/a> <a c/', $people_html);
+for ($page = 2; $page < $count; $page++) {
+	$people_html = fetch("https://github.com/orgs/$org/people?page=$page");
+	preg_match_all("/f=\"\/orgs\/$org\/people\/(.*)\"></", $people_html, $people);
+	foreach ($people[1] as $i => $person) {
+		$contributors[$person]["💼"] = 1;
+	}
+}
 
+unset($i);
+unset($page);
+unset($count);
+unset($person);
+unset($people);
+unset($people_html);
 
 $repos = json_decode(fetch("https://api.github.com/orgs/$org/repos"));
 foreach ($repos as $r) {
@@ -50,11 +38,37 @@ foreach ($repos as $r) {
 	$pulls       = fetch("https://api.github.com/repos/$org/$repo/pulls");
 	$discussions = fetch("https://github.com/$org/$repo/discussions?discussions_q=is%3Aanswered");
 	$wiki        = fetch("https://github.com/$org/$repo/wiki/_history");
-
-	
 }
 
-print_r($contributors);
+output_page($contributors);
 
+function output_page($out)
+{
+	global $time_start;
+	$time_end      = microtime(TRUE);
+	$out["__TIME"] = $time_end - $time_start;
+	echo json_encode($out);
+}
 
-//output_page($contributors);
+function fetch($url)
+{
+	$ch = curl_init($url);
+	if ($ch === FALSE) {
+		return FALSE;
+	}
+
+	curl_setopt_array($ch, array(
+		CURLOPT_RETURNTRANSFER => TRUE,
+		CURLOPT_FOLLOWLOCATION => TRUE,
+		CURLOPT_SSL_VERIFYPEER => FALSE,
+		CURLOPT_USERAGENT      => 'RedGuy12',
+		CURLOPT_COOKIE         => "user_session=G9FxEJnxYRJuYeRDHMEiVHWdKamTuHHVxZyOi8b_Jzl_GqU6"
+	));
+	$out = curl_exec($ch);
+	curl_close($ch);
+	if ($out === FALSE) {
+		return $ch;
+	}
+
+	return $out;
+}
